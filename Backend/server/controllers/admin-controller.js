5edfadc0e4ce2df5549974f798249aec09d74501
juned1110 <junedking1110@gmail.com>
2024-06-24 15:01:40 +0530

    admin controller contact logic updated

--- Backend/server/controllers/admin-controller.js
+++ Backend/server/controllers/admin-controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user-model");
+const Contact = require("../models/contact-model");
 
+//Getting all user logic
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.find({}, { password: 0 });
@@ -13,4 +15,17 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-module.exports = getAllUsers;
+//getting all contacts logic
+const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    if (!contacts || contacts.length === 0) {
+      return res.status(404).json({ message: "contacts not found" });
+    }
+    return res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getAllUsers, getAllContacts };
