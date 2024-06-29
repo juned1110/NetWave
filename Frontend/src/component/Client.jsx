de616646d8751caa4551686cdcece204bc7b9674
juned1110 <junedking1110@gmail.com>
2024-06-29 12:25:57 +0530

    minor bug fixes in client

--- Frontend/src/component/Client.jsx
+++ Frontend/src/component/Client.jsx
@@ -8,16 +8,18 @@ import img6 from "../assets/logo-6.png";
 
 const Client = () => {
   return (
-    <div className="w-full h-52">
-      <div className="img flex mt-14 justify-evenly h-[26vh]">
-        <img src={img1} alt="" />
-        <img src={img2} alt="" />
-        <img src={img3} alt="" />
-        <img src={img4} alt="" />
-        <img src={img5} alt="" />
-        <img src={img6} alt="" />
+    <>
+      <div className="w-full h-[40vh] p-1 bg-[#03E9F4]">
+        <div className="img flex mt-14 justify-evenly h-[26vh] items-center">
+          <img src={img1} alt="" />
+          <img src={img2} alt="" />
+          <img src={img3} alt="" />
+          <img src={img4} alt="" />
+          <img src={img5} alt="" />
+          <img src={img6} alt="" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
