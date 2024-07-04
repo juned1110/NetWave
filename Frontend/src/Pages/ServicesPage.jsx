387bc877cc7bee0e1c628c8b2effc9164aff60de
juned1110 <junedking1110@gmail.com>
2024-07-04 15:57:02 +0530

    responsive added to service page

--- Frontend/src/Pages/ServicesPage.jsx
+++ Frontend/src/Pages/ServicesPage.jsx
@@ -9,7 +9,7 @@ import img5 from "../assets/card5.png";
 import img6 from "../assets/card6.png";
 
 const ServicesPage = () => {
-  const { user, isLoggedIn, loading } = useAuth();
+  const { user, isLoggedIn } = useAuth();
   const [loadingImages, setLoadingImages] = useState(true);
   const { services } = useAuth();
 
@@ -39,8 +39,8 @@ const ServicesPage = () => {
   return (
     <>
       <GoogleFontLoader fonts={[{ font: "Play", weights: [400, 700] }]} />
-      <div className="relative w-full h-auto bg-gradient-to-r from-[#ffffff] to-[#ECE9E6]">
-        <div className="absolute mt-5 left-14 w-[11rem] py-4 rounded-md bg-[#03E9F4] text-center text-black font-bold text-sm">
+      <div className="relative w-full h-auto">
+        <div className="absolute md:mb-10 left-4 md:left-14 w-[11rem] py-4 rounded-md bg-[#03E9F4] text-center text-black font-bold text-sm">
           <div
             className="flex justify-center items-center h-full"
             style={{ fontFamily: "Play" }}
@@ -49,18 +49,18 @@ const ServicesPage = () => {
           </div>
         </div>
         <div
-          className="absolute mt-20 left-14 text-4xl text-black font-bold"
+          className="absolute mt-20 left-4 md:left-14 text-2xl md:text-4xl text-black font-bold"
           style={{ fontFamily: "Play" }}
         >
           We are Professional Internet Service
           <p style={{ fontFamily: "Play" }}>Provider.</p>
         </div>
 
-        <div className="grid grid-cols-3 gap-12 p-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 md:p-12 mt-36 md:mt-28">
           {serviceImg.map((imgSrc, index) => (
             <div
               key={index}
-              className="relative rounded-lg h-[65vh] border-2 flex flex-col items-center justify-center mt-36"
+              className="relative rounded-lg h-auto border-2 flex flex-col items-center justify-center mt-32"
               style={{
                 boxShadow:
                   "rgba(0, 0, 0, 0.17) 0px -2px 2px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -7px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 1px 2px, rgba(0, 0, 0, 0.09) 0px 2px 4px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 10px 10px",
